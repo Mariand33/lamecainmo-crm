0d030dde12bcb770100ae7b3b576bea6ca1bf38a
Mariand33 <invictussdigital@gmail.com>
2026-04-23 19:37:00 -0300

    Change static file directory from 'public' to 'Público'

--- server.js
+++ server.js
@@ -106,8 +106,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
-
+app.use(express.static(path.join(__dirname, "Público")));
 app.use(
   session({
     secret: "buzzacchi",
