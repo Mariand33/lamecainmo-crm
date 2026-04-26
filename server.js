67400a08a6d2c158902e8e16a98be162ebc85b9b
Mariand33 <invictussdigital@gmail.com>
2026-04-26 11:55:58 -0300

    Add static file serving for 'public' directory

--- server.js
+++ server.js
@@ -107,6 +107,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "Público")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
     secret: "buzzacchi",
