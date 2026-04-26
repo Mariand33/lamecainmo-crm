1e414aeb17d465f10df525c22da1f3a62f5fe373
Mariand33 <invictussdigital@gmail.com>
2026-04-26 16:31:54 -0300

    Serve static files from 'Público' directory

--- server.js
+++ server.js
@@ -106,6 +106,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "Público")));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
@@ -257,7 +258,7 @@ function inmToSb(inm) {
 // ============================
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "dashboard.html"));
+  res.sendFile(path.join(__dirname, "Público", "dashboard.html"));
 });
 
 app.get("/demandas", (req, res) => {
